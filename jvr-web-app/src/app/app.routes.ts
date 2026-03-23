a21050f269ae7b61a582b4bafd2b786af7739ebd
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2026-03-23 15:00:19 -0400

    Slightly optimize page

--- jvr-web-app/src/app/app.routes.ts
+++ jvr-web-app/src/app/app.routes.ts
@@ -1,20 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { LandingPage } from './pages/landing-page/landing-page';
-import { Homepage } from './pages/homepage/homepage';
-import { ServicesPage } from './pages/services-page/services-page';
-import { WhyUsPage } from './pages/why-us-page/why-us-page';
-import { AboutUs } from './pages/about-us/about-us';
-import { ContactPage } from './pages/contact-page/contact-page';
-import { CaseStudy } from './pages/case-study/case-study';
 
 export const routes: Routes = [
   { path: '', component: LandingPage },
-  { path: 'home', component: Homepage },
-  { path: 'services', component: ServicesPage },
-  { path: 'why-us', component: WhyUsPage },
-  { path: 'about', component: AboutUs },
-  { path: 'contact', component: ContactPage },
-  { path: 'case-studies', component: CaseStudy },
+  { path: 'home',         loadComponent: () => import('./pages/homepage/homepage').then(m => m.Homepage) },
+  { path: 'services',     loadComponent: () => import('./pages/services-page/services-page').then(m => m.ServicesPage) },
+  { path: 'why-us',       loadComponent: () => import('./pages/why-us-page/why-us-page').then(m => m.WhyUsPage) },
+  { path: 'about',        loadComponent: () => import('./pages/about-us/about-us').then(m => m.AboutUs) },
+  { path: 'contact',      loadComponent: () => import('./pages/contact-page/contact-page').then(m => m.ContactPage) },
+  { path: 'case-studies', loadComponent: () => import('./pages/case-study/case-study').then(m => m.CaseStudy) },
   { path: '**', redirectTo: '' },
 ];
